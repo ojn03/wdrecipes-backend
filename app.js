c0aa79897f00b28a89a3d7065673bff80cea3283
Andrew Yang <86063610+AndrewY0433@users.noreply.github.com>
2023-12-14 05:22:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
 	cors({
 		credentials: true,
-		origin: process.env.FRONTEND_URL
+        	origin: [process.env.FRONTEND_URL, 'http://localhost:3000']
 	})
 );
 const sessionOptions = {
